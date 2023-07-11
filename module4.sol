17ed297a82f37b7af4e806862fae82429967f033
kumkumsharma06 <135731376+kumkumsharma06@users.noreply.github.com>
2023-07-11 10:42:12 +0000

    module4.sol

--- module4.sol
+++ module4.sol
@@ -1,1 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract DegenToken is ERC20, ERC20Burnable, Ownable {
+    constructor() ERC20("DegenToken", "DGN") {
+        _mint(msg.sender, 10000 * 10 ** decimals());
+    }
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+    function transfer(address to, uint256 amount) public virtual override returns (bool) {
+        _transfer(_msgSender(), to, amount);
+        return true;
+    }
+    function redeem(uint256 amount) public {
+        _burn(_msgSender(), amount);
+    }
+    function balanceOf(address account) public view override returns (uint256) {
+        return super.balanceOf(account);
+    }
+}
 
