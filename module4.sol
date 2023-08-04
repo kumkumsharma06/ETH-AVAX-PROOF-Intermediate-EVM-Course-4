7f8e4a8fe352a25437a4e63ea5a4d6d7497fba7a
kumkumsharma06 <135731376+kumkumsharma06@users.noreply.github.com>
2023-08-04 17:29:57 +0530

    module4.sol

--- module4.sol
+++ module4.sol
@@ -6,34 +6,30 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DegenToken is ERC20, ERC20Burnable, Ownable {
-    constructor() ERC20("DegenToken", "DGN") {}
+    mapping(string => uint256) private rewardCosts;
+    constructor() ERC20("DegenToken", "DGN") {
+        _mint(msg.sender, 10000 * 10 ** decimals());
+        rewardCosts["Reward1"] = 100;
+        rewardCosts["Reward2"] = 200;
+    }
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
     function transfer(address to, uint256 amount) public virtual override returns (bool) {
         _transfer(_msgSender(), to, amount);
-        approve(msg.sender, amount);
         return true;
     }
-     function balanceOf(address account) public view override returns (uint256) {
+    function redeem(string memory rewardName) public {
+        uint256 cost = rewardCosts[rewardName];
+        require(cost > 0, "Reward not available");
+        require(balanceOf(msg.sender) >= cost, "Insufficient balance");
+        _burn(msg.sender, cost);
+        emit RewardRedeemed(msg.sender, rewardName);
+    }
+    function balanceOf(address account) public view override returns (uint256) {
         return super.balanceOf(account);
     }
-    function DegenGame() public pure returns(string memory) {
-            return "1. GAMER NFT value = 500 \n 2. DegenGamer value = 175";
-        }
-        function reedem(uint choice) external payable{
-           // require(choice<=2,"your selection is invalid for this");
-            if(choice ==1){
-                require(balanceOf(msg.sender)>=500, "Insufficient Balance For Your Selection");
-                approve(msg.sender, 500);
-                transferFrom(msg.sender, owner(), 500);
-            }
-            
-            else{
-                require(balanceOf(msg.sender)>=175, "Insufficient Balance For Your Selection");
-                approve(msg.sender, 175);
-                transferFrom(msg.sender, owner(), 175);
-            }
-        }
+    event RewardRedeemed(address indexed account, string rewardName);
 }
+
 
